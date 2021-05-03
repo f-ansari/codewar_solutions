c8fe9ee98e59272fc232589cdccdfd3e772d8392
Faryal Ansari <faryalansari@live.com>
2021-05-03 13:26:19 -0400

    Revert "Solved: Covert Boolean values to strings 'Yes' or 'No' > line 1-9"
    This reverts commit 2e848712856c1955c8a3bf662a076dc7aadc8069.

--- javaScript/8_kyu.js
+++
@@ -1,9 +0,0 @@
-// Problem: Covert Boolean values to strings 'Yes' or 'No'.
-// https://www.codewars.com/kata/53369039d7ab3ac506000467/train/javascript
-// Solved
-
-const boolToWord = (bool) => {
-  return bool ? 'Yes' : 'No'
-}
-
-// console.log(boolToWord(false))
