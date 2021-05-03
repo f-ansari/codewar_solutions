8a6af464acbd2c13544a54481eebebcaa8bcd711
Faryal Ansari <faryalansari@live.com>
2021-05-03 13:32:10 -0400

    [8_kyu] Solved: Covert Boolean values to strings 'Yes' or 'No' > line 1-9

---
+++ javaScript/8_kyu.js
@@ -0,0 +1,9 @@
+// Problem: Conver boolean values to dtrings 'Yes' or 'No'.
+// https://www.codewars.com/kata/53369039d7ab3ac506000467/train/javascript
+// Solved
+
+const boolToWord = (bool) => {
+  return bool ? 'Yes' : 'No'
+}
+
+console.log(boolToWord(false))
