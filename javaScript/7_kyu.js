5a2fd1e5b4e7c817d772b4fe6012fc341b476bb9
Faryal Ansari <faryalansari@live.com>
2021-05-04 19:17:34 -0400

    [7_kyu] Solved: Vowel Count > line 1-22 (refactored)

--- javaScript/7_kyu.js
+++ javaScript/7_kyu.js
@@ -1,27 +1,6 @@
 // Problem: Vowel Count
 // https://www.codewars.com/kata/54ff3102c1bad923760001f3/train/javascript
-// Solved: need to refactor
-
-// const getCount = (str) => {
-//   let vowelsCount = 0
-//   const charArray = str.split('')
-//   for (let i = 0; i < charArray.length; i++) {
-//     if (
-//       charArray[i].includes('a') ||
-//       charArray[i].includes('e') ||
-//       charArray[i].includes('i') ||
-//       charArray[i].includes('o') ||
-//       charArray[i].includes('u') === true
-//     ) {
-//       vowelsCount += 1
-//     }
-//   }
-//   //   console.log(vowelsCount)
-// }
-
-// // getCount('abracadabra')
-// // getCount('pear tree')
-// getCount('The quick brown fox jumps over the lazy dog')
+// Solved
 
 const getCount = (str) => {
   let vowelsCount = 0
