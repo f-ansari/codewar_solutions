652e56f0d19aefbf696ad25a2afb8f6cedebe000
Faryal Ansari <faryalansari@live.com>
2021-05-04 18:16:06 -0400

    set up problem

---
+++ javaScript/7_kyu.js
@@ -0,0 +1,9 @@
+// Problem: Vowel Count
+// https://www.codewars.com/kata/54ff3102c1bad923760001f3/train/javascript
+// Not Solved
+
+const getCount = (str) => {
+  let vowelsCount = 0
+}
+
+getCount('abracadabra')
