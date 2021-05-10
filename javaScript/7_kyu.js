2eac9f5291ca2fe936d044cef3c99bc4c4fd3b03
Faryal Ansari <faryalansari@live.com>
2021-05-09 23:44:16 -0400

    [7_kyu] solved question starting line 24 ... need to refactor

--- javaScript/7_kyu.js
+++ javaScript/7_kyu.js
@@ -19,14 +19,29 @@ const getCount = (str) => {
 
 // getCount('abracadabra')
 // getCount('pear tree')
-getCount('The quick brown fox jumps over the lazy dog')
+// getCount('The quick brown fox jumps over the lazy dog')
 
 // Problem: Reverse a Number
 // https://www.codewars.com/kata/555bfd6f9f9f52680f0000c5/train/javascript
 // not solved
 
-const reverseNumber = (num) => {}
+const reverseNumber = (num) => {
+  let n = num.toString()
+  let nArray = n.split('')
+  let resultArray = []
+  let negHolder = ''
+
+  nArray.forEach((element) => {
+    if (element === '-') {
+      negHolder = element
+    } else resultArray.push(element)
+  })
+  resultArray.reverse()
+  resultArray.join('')
+
+  console.log(parseInt(negHolder + resultArray.join('')))
+}
 
 reverseNumber(123)
-reverseNumber(-543)
-reverseNumber(876)
+// reverseNumber(-543)
+// reverseNumber(876)
