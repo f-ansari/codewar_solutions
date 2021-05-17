6c23985495acce3eab4b5bd848601fc29b3ec19a
Faryal Ansari <faryalansari@live.com>
2021-05-16 23:41:54 -0400

    [7_kyu] setup problem on line 66

--- javaScript/7_kyu.js
+++ javaScript/7_kyu.js
@@ -62,3 +62,11 @@ const findShort = (s) => {
 }
 
 findShort('bitcoin take over the world maybe who knows perhaps')
+
+// Problem: Highest and Lowest
+// https://www.codewars.com/kata/554b4ac871d6813a03000035/train/javascript
+// not solved
+
+const highAndLow = (number) => {}
+
+highAndLow('4 5 29 54 4 0 -214 542 -64 1 -3 6 -6')
